d7acbffbfc65cfea693ef4829af16544ac700b90
Luyang <rend1820@pop-os.localdomain>
2021-12-30 18:21:52 +1100

    Added recall python code

---
+++ machine-learning/accuracy,recall,precision,f1-score/recall.py
@@ -0,0 +1,23 @@
+labels = [1, 0, 0, 1, 1, 1, 0, 1, 1, 1]
+guesses = [0, 1, 1, 1, 1, 0, 1, 0, 1, 0]
+
+
+true_positives = 0
+true_negatives = 0
+false_positives = 0
+false_negatives = 0
+
+assert len(labels) == len(guesses), "must be the same length"
+
+for i in range(len(labels)):
+    if labels[i] == 1 and guesses[i] == 1:
+        true_positives +=1
+    elif labels[i] == 0 and guesses[i] == 1:
+        false_positives +=1
+    elif labels[i] == 0 and guesses[i] == 0:
+        true_negatives +=1
+    elif labels[i] == 1 and guesses[i] == 0:
+        false_negatives +=1
+
+recall = (true_positives)/(true_positives + false_negatives)
+print(recall)
