07147119977c2ade6b6d06a930c479e571c70309
Luyang Liu <https://github.com/luyangliuable>
2022-02-07 13:05:05 +1100

    added sorting algorithms

---
+++ algorithms/insertion-sort.py
@@ -0,0 +1,19 @@
+def insertion_sort(arr):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+  
+        key = arr[i]
+  
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i-1
+        while j >=0 and key < arr[j]:
+                arr[j+1] = arr[j]
+                j -= 1
+        arr[j+1] = key
+  
+    return arr
+
+
+print(insertion_sort([1,2,23,2,3,1,2,3,4,5,7,8,8]))
