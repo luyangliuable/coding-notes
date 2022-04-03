af1f75c7e686c42254eabd63c49b8bf42537d8c1
Luyang Liu <luyangl16@gmail.com>
2022-04-03 21:20:35 +1000

    Added more coding notes on machine learning and updated old code

--- algorithms/merge_sort.py
+++ algorithms/merge_sort.py
@@ -1,13 +1,14 @@
 def mergesort(arr):
-    if len(arr) > 1:
-        # Finding the mid of the array
-        mid = len(arr)//2
+    res = arr
+    if len(res) > 1:
+        # Finding the mid of the resay
+        mid = len(res)//2
   
-        # Dividing the array elements
-        L = arr[:mid]
+        # Dividing the resay elements
+        L = res[:mid]
   
         # into 2 halves
-        R = arr[mid:]
+        R = res[mid:]
   
         # Sorting the first half
         mergesort(L)
@@ -17,24 +18,24 @@ def mergesort(arr):
   
         i = j = k = 0
   
-        # Copy data to temp arrays L[] and R[]
+        # Copy data to temp resays L[] and R[]
         while i < len(L) and j < len(R):
             if L[i] < R[j]:
-                arr[k] = L[i]
+                res[k] = L[i]
                 i += 1
             else:
-                arr[k] = R[j]
+                res[k] = R[j]
                 j += 1
             k += 1
   
         # Checking if any element was left
         while i < len(L):
-            arr[k] = L[i]
+            res[k] = L[i]
             i += 1
             k += 1
   
         while j < len(R):
-            arr[k] = R[j]
+            res[k] = R[j]
             j += 1
             k += 1
   
