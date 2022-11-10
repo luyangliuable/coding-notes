b9af85141778a49d3343cb88d8602c379e2fa6d6
Luyang Liu <luyangl16@gmail.com>
2022-11-10 17:12:52 +1100

    Added notes on machine learning perceptrons

---
+++ machine-learning/perceptron/training_perceptron.py
@@ -0,0 +1,39 @@
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import random
+
+def generate_training_set(num_points):
+	x_coordinates = [random.randint(0, 50) for i in range(num_points)]
+	y_coordinates = [random.randint(0, 50) for i in range(num_points)]
+	training_set = dict()
+	for x, y in zip(x_coordinates, y_coordinates):
+		if x <= 45-y:
+			training_set[(x,y)] = 1
+		elif x > 45-y:
+			training_set[(x,y)] = -1
+	return training_set
+
+training_set = generate_training_set(30)
+
+x_plus = []
+y_plus = []
+x_minus = []
+y_minus = []
+
+for data in training_set:
+	if training_set[data] == 1:
+		x_plus.append(data[0])
+		y_plus.append(data[1])
+	elif training_set[data] == -1:
+		x_minus.append(data[0])
+		y_minus.append(data[1])
+
+fig = plt.figure()
+ax = plt.axes(xlim=(-25, 75), ylim=(-25, 75))
+
+plt.scatter(x_plus, y_plus, marker = '+', c = 'green', s = 128, linewidth = 2)
+plt.scatter(x_minus, y_minus, marker = '_', c = 'red', s = 128, linewidth = 2)
+
+plt.title("Training Set")
+
+plt.show()
