35beb206a3f75c2c9c6c1f415456e91df3e45b45
Luyang Liu <luyangl16@gmail.com>
2022-11-18 10:50:22 +1100

    Added notes on typescript union type

---
+++ typescript/union_types/inferred_union_return_types.ts
@@ -0,0 +1,18 @@
+type User = {
+    id: number;
+    username: string;
+};
+
+
+function createUser() {
+    const randomChance = Math.random() >= 0.5;
+
+    if (randomChance) {
+        return { id: 1, username: 'nikko' };
+    } else {
+        return 'Could not create a user.';
+    }
+}
+
+
+let userData: User | string = createUser();
