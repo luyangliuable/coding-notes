a74d02ed1f859440e8e6639b21a4f01d12734bf9
Luyang Liu <luyangl16@gmail.com>
2023-01-03 13:40:47 +1100

    Added some notes on software security

---
+++ cybersecurity/software_security/labs/lab1/data_types_and_syntax.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+
+  int a = 10;        // an integer variable
+  float pie = 3.14;  // a float one
+  double pie2 = 3.14; // a double one
+  /* bool bo = true;    // a boolean one */
+  char c = 'a';      // a character variable
+  int a2[10];         // define an array of integer type, length is 10
+  return 0;
+}
