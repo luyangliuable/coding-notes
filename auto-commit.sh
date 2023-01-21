385a3afa645ac49ef9c2f9eb00bf0418b07d9135
Luyang L <luyangl16@gmail.com>
2023-01-21 16:50:59 +1100

    Modified notes auto-commit.sh.

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
+# Function to automatically commit changes to a git repository
 function auto_commit() {
+    # Print the received argument
+    echo "Received argument $1"
+
+    # Get the list of modified files
     modified=$(git status | awk '/modified:/ { print $2 }')
 
+    # Get the list of untracked files
     untracked=$(git status --porcelain | awk '/^\?\?/ { print $2 }')
 
-    gitmessage="Added notes on $untracked. Modified notes $modified."
+    # Create commit message with the modified and untracked files
+    if [ -z "$untracked" ] && [ -n "$modified" ]; then
+        gitmessage="Modified notes $modified."
+    elif [ -n "$untracked" ] && [ -n "$modified" ]; then
+        gitmessage="Added notes on $untracked. Modified notes $modified."
+    elif [ -n "$untracked" ] && [ -z "$modified" ]; then
+        gitmessage="Added notes on $untracked."
+    fi
+
     echo $gitmessage
 
+    Add all changes
     git add .
+    Commit with the generated message
     git commit -m "$gitmessage"
+    Push to remote repository
     git push
 }
 
+# Call the function with no arguments
 auto_commit
