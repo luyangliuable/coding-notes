3c96edb605dab286c95458ba49d6f0c5e872e68d
Luyang Liu <luyangl16@gmail.com>
2023-01-25 22:30:00 +1100

    Autocommit: Modified rust/exercism_learning/src/arithmetic/assembly_line.rs rust/exercism_learning/src/main.rs.

--- rust/exercism_learning/src/arithmetic/assembly_line.rs
+++ rust/exercism_learning/src/arithmetic/assembly_line.rs
@@ -5,13 +5,23 @@
 pub fn production_rate_per_hour(speed: u8) -> f64 {
     // unimplemented!("calculate hourly production rate at speed: {}", speed)
     let mut a = (speed as f64);
-    match speed {
-        ( speed <= 4 ):
+    if ( a >= 0.0 && a <= 4.0 ) {
+        a = a * (221 as f64);
+    } else if (a > 4.0 && a <= 8.0) {
+        a = a * (221 as f64)*0.90;
+    } else if (a > 8.0 && a <= 10.0) {
+        a = a * (221 as f64)*0.77;
+    } else {
+        panic!("Invalid speed");
     }
     println!("The value of a is: {}", a);
     a
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    // unimplemented!("calculate the amount of working items at speed: {}", speed)
+    let cars = production_rate_per_hour(speed as u8) as u32;
+    let cars_per_minute = cars / 60;
+    println!("cars per minute is: {}", cars_per_minute);
+    cars_per_minute
 }
--- rust/exercism_learning/src/main.rs
+++ rust/exercism_learning/src/main.rs
@@ -9,5 +9,6 @@ use enums::semi_structured_logs::{log, info, error, warn, LogLevel};
 // functions::lucian_luscious_lasagna;
 
 fn main() {
-    arithmetic::assembly_line::production_rate_per_hour(12);
+    arithmetic::assembly_line::production_rate_per_hour(6);
+    arithmetic::assembly_line::working_items_per_minute(6);
 }
