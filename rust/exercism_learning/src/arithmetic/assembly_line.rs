3ad8fb215d730748eaafb06b3a8d2092d523b848
Luyang Liu <luyangl16@gmail.com>
2023-01-25 22:00:00 +1100

    Autocommit: Added notes on rust/exercism_learning/src/arithmetic/. Modified notes rust/exercism_learning/src/main.rs.

---
+++ rust/exercism_learning/src/arithmetic/assembly_line.rs
@@ -0,0 +1,17 @@
+// This stub file contains items that aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    // unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let mut a = (speed as f64);
+    match speed {
+        ( speed <= 4 ):
+    }
+    println!("The value of a is: {}", a);
+    a
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    unimplemented!("calculate the amount of working items at speed: {}", speed)
+}
