66a0558f9d16db8c75c51f0611cb95f8bc90e4e8
Luyang Liu <luyangl16@gmail.com>
2023-01-28 08:54:00 +1100

    Autocommit: Added notes on vim-and-emacs/emacslisp/string/two-fer.lisp. Modified notes auto-commit.

---
+++ vim-and-emacs/emacslisp/string/two-fer.lisp
@@ -0,0 +1,14 @@
+;;; two-fer.el --- Two-fer Exercise (exercism)
+;;; Commentary:
+
+
+;;; Code:
+(defun two-fer (&optional name)
+  (if (null name)
+    "One for ~A, one for me"
+    (format "One for %s, one for me." name)
+  )
+)
+
+(provide 'two-fer)
+;;; two-fer.el ends here
