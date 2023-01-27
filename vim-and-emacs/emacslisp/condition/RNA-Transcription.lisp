e5f3e52ed702403a99ee35e339b9b6992a6d4e9a
Luyang Liu <luyangl16@gmail.com>
2023-01-28 09:50:01 +1100

    Autocommit: Added notes on vim-and-emacs/emacslisp/condition/RNA-Transcription.lisp. Modified notes auto-commit.

--- vim-and-emacs/emacslisp/condition/RNA-Transcription.lisp
+++ vim-and-emacs/emacslisp/condition/RNA-Transcription.lisp
@@ -7,11 +7,16 @@
     (dotimes (i (length dna-strand))
       (let ((nucleotide (aref dna-strand i)))
         (cond
-          ((char-equal nucleotide #\G) (setq rna-strand (concat rna-strand "C")))
-          ((char-equal nucleotide #\C) (setq rna-strand (concat rna-strand "G")))
-          ((char-equal nucleotide #\T) (setq rna-strand (concat rna-strand "A")))
-          ((char-equal nucleotide #\A) (setq rna-strand (concat rna-strand "U"))))))
-    rna-strand))
+          ((char-equal nucleotide ?G) (setq rna-strand (concat rna-strand "C")))
+          ((char-equal nucleotide ?C) (setq rna-strand (concat rna-strand "G")))
+          ((char-equal nucleotide ?T) (setq rna-strand (concat rna-strand "A")))
+          ((char-equal nucleotide ?A) (setq rna-strand (concat rna-strand "U")))
+        )
+      )
+    )
+    rna-strand
+  )
+)
 
 (defun to-rna-one-char (char)
   (cond ((string= strand 'A)
