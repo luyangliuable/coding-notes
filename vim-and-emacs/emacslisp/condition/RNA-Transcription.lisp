d61e0c21c190cb1900cec8b1676a8ccff7105efc
Luyang Liu <luyangl16@gmail.com>
2023-01-28 09:36:00 +1100

    Autocommit: Added notes on vim-and-emacs/emacslisp/condition/. Modified notes auto-commit.

---
+++ vim-and-emacs/emacslisp/condition/RNA-Transcription.lisp
@@ -0,0 +1,14 @@
+;;; rna-transcription.el -- RNA Transcription (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun to-rna (strand)
+  ;;; Code:
+  (cond ((string= strand 'A)
+         ()
+    )
+  )
+)
+
+(provide 'rna-transcription)
+;;; rna-transcription.el ends here
