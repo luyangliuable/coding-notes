32afb2c65dbbed290e6e70c475acbd3cb9d4a829
Luyang Liu <luyangl16@gmail.com>
2023-01-28 09:55:01 +1100

    Autocommit: Added notes on vim-and-emacs/emacslisp/condition/RNA-Transcription.el. Modified notes auto-commit.

--- vim-and-emacs/emacslisp/condition/RNA-Transcription.el
+++ vim-and-emacs/emacslisp/condition/RNA-Transcription.el
@@ -7,17 +7,25 @@
     (dotimes (i (length dna-strand))
       (let ((nucleotide (aref dna-strand i)))
         (cond
-          ((char-equal nucleotide ?G) (setq rna-strand (concat rna-strand "C")))
-          ((char-equal nucleotide ?C) (setq rna-strand (concat rna-strand "G")))
-          ((char-equal nucleotide ?T) (setq rna-strand (concat rna-strand "A")))
-          ((char-equal nucleotide ?A) (setq rna-strand (concat rna-strand "U")))
-          ((error "Invalid char: %s" nucleotide))
+         ;; char-equal is used here to compare the character
+         ;; because it is case-insensitive, whereas char= is case-sensitive
+         ;; ?G, ?C, ?T, ?A are used as shortcut to represent #\G, #\C, #\T, #\A respectively
+         ((char-equal nucleotide ?G) (setq rna-strand (concat rna-strand "C")))
+         ((char-equal nucleotide ?C) (setq rna-strand (concat rna-strand "G")))
+         ((char-equal nucleotide ?T) (setq rna-strand (concat rna-strand "A")))
+         ((char-equal nucleotide ?A) (setq rna-strand (concat rna-strand "U")))
+         ;; error function is used to raise an error with a custom message
+         ((error "Invalid char: %s" nucleotide))
+         )
         )
       )
-    )
     rna-strand
+    )
   )
-)
+
+
+
+
 
 (defun to-rna-one-char (char)
   (cond ((string= strand 'A)
