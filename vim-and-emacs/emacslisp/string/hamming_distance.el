eba645d0f9ffd8f74ef4298d75d7e179bd4bdf9b
Luyang Liu <luyangl16@gmail.com>
2023-01-29 09:00:01 +1100

    Autocommit: Added notes on vim-and-emacs/emacslisp/string/hamming_distance.el. Modified notes auto-commit.

---
+++ vim-and-emacs/emacslisp/string/hamming_distance.el
@@ -0,0 +1,10 @@
+;;; hamming.el --- Hamming (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun hamming-distance (dna1 dna2)
+;;; Code:  
+  )
+
+(provide 'hamming)
+;;; hamming.el ends here
