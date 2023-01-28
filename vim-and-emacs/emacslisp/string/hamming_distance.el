84f2e8ee4b709da30c48ec4ded23ab1b3a5d28e3
Luyang Liu <luyangl16@gmail.com>
2023-01-29 09:24:01 +1100

    Autocommit: Added notes on vim-and-emacs/emacslisp/condition/RNA-Transcription.el. Modified notes auto-commit.

--- vim-and-emacs/emacslisp/string/hamming_distance.el
+++ vim-and-emacs/emacslisp/string/hamming_distance.el
@@ -1,26 +1,29 @@
 ;;; hamming.el --- Hamming (exercism)  -*- lexical-binding: t; -*-
-
+;;; Author: Luyang Liu
 ;;; Commentary:
+;;; This file contains a function that calculates the Hamming distance
+;;; between two DNA strings. The Hamming distance is the number of
+;;; positions at which the corresponding symbols are different.
+;;; Code:
 
 (defun hamming-distance (dna1 dna2)
-  ;;; Code:  
-  (if (not (eq (length dna1) (length dna2)))
-      (error "Not same length")
-  )
-
-  (let ((dist 0))
-    (dotimes (i (min (length dna1) (length dna2)) )
-      (when (not (eq (aref dna1 i) (aref dna2 i)))
-        (setq dist (+ dist 1))
-      )
-    )
-    (progn
+  "Calculate the Hamming distance between DNA strings DNA1 and DNA2.
+  If the strings are of different length, return an error."
+  (if (not (= (length dna1) (length dna2)))
+      (error "Strings are not of equal length")
+    (let ((dist 0))
+      (dotimes (i (length dna1))
+        ;; increment dist if characters at position i are different
+        (when (not (eq (aref dna1 i) (aref dna2 i)))
+          (setq dist (+ dist 1))
+          )
+        )
       (print dist)
       dist
+      )
     )
   )
-)
 
 (provide 'hamming)
-(hamming-distance "llo" "bye")
+(print (hamming-distance "llo" "bye"))
 ;;; hamming.el ends here
