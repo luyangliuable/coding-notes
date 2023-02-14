902a2fd254d092fccc04637dbe9a66f9e1c54f44
Luyang Liu <luyangl16@gmail.com>
2023-02-15 09:32:57 +1100

    Added notes on threat modelling

---
+++ c#/test/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Out.WriteLine("This is the miracle");
+        Worker worker1 = new Worker();
+        Worker worker2 = new Worker();
+
+        if (worker1 == worker2) {
+            Console.Out.WriteLine("Equals!");
+        }
+    }
+
+    Interface Vehicle {
+        public Worker() {
+            ;
+        }
+    }
+}
