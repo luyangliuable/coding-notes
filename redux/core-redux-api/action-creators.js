d0ef41ad407269f727dce16aea3d0fef4841b02a
Luyang Liu <luyangl16@gmail.com>
2023-02-19 20:00:28 +1100

    Autocommit: Added notes on redux/core-redux-api/action-creators.js. Modified notes auto-commit.

--- redux/core-redux-api/action-creators.js
+++ redux/core-redux-api/action-creators.js
@@ -1,6 +1,13 @@
 import { createStore } from 'redux';
 
 // Create your action creators here.
+const increment = () => {
+    return { type: 'increment'};
+};
+
+const decrement = () => {
+    return { type: 'decrement'};
+};
 
 const initialState = 0;
 const countReducer = (state = initialState, action) => {
@@ -17,11 +24,11 @@ const countReducer = (state = initialState, action) => {
 const store = createStore(countReducer);
 
 // Modify the dispatches below.
-store.dispatch({ type: 'increment' });
-store.dispatch({ type: 'increment' });
+store.dispatch(increment());
+store.dispatch(increment());
 console.log(store.getState());
 
-store.dispatch({ type: 'decrement' });
-store.dispatch({ type: 'decrement' });
-store.dispatch({ type: 'decrement' });
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(decrement());
 console.log(store.getState());
