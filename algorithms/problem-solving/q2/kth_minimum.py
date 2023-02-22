e5c8a0e978ac93c033ab034e012422bbce44d17e
Luyang Liu <luyangl16@gmail.com>
2023-02-22 20:30:01 +1100

    Autocommit: Added notes on algorithms/problem-solving/q2/. Modified notes auto-commit.

--- algorithms/problem-solving/q2/kth_minimum.py
+++ algorithms/problem-solving/q2/kth_minimum.py
@@ -7,18 +7,22 @@ def getkthSmallestTerm(arr, k):
     # Check which first number
     f_idx = k // len(arr)
 
-    # sort input arr
-    arr.sort()
-
     # Get the second term index
     s_idx = k - f_idx*len(arr) - 1
 
-    for i in range(f_idx, len(arr)):
-        for j in range(len(arr)):
-            a.append([arr[f_idx], arr[j]])
+    # Count number of f_idx numbers
+    c = 0
 
-    print(a)
+    for i in range(len(arr)):
+        if arr[i] == arr[f_idx-1]:
+            c += 1
+
+    for i in range(f_idx-1, f_idx+c-1):
+        for j in range(len(arr)):
+            a.append([arr[i], arr[j]])
+
     a.sort()
+    print(a)
 
     r = a[s_idx]
 
@@ -30,5 +34,5 @@ def getkthSmallestTerm(arr, k):
 # 1,2 1,2 2,1 2,1 2,2 2,2
 cp_arr = [2, 2, 1]
 cp_arr2 = [4, 1]
-# print(getkthSmallestTerm(cp_arr, 5))
+print(getkthSmallestTerm(cp_arr, 5))
 print(getkthSmallestTerm(cp_arr2, 3))
