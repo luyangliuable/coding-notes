2ebf8506cff8e36f70b656af04da12fa592c67fe
Luyang Liu <luyangl16@gmail.com>
2023-04-30 10:00:01 +1000

    Autocommit: Added notes on auto-commit. Modified notes .gitignore.

---
+++ computer-networks/layers-from-osi-model/network/subnet-masks/ip-address-range-from-cidr-range.py
@@ -0,0 +1,17 @@
+import ipaddress
+
+ip = "192.168.1.1"
+
+for cidr in range(32, -1, -1):
+    network = ipaddress.IPv4Network(f"{ip}/{cidr}", strict=False)
+    network_address = network.network_address
+    broadcast_address = network.broadcast_address
+    usable_start = network_address + 1
+    usable_end = broadcast_address - 1
+
+    print(f"{network_address}	{broadcast_address}		{usable_start}	{usable_end}")
+
+    # print(f"CIDR: {cidr}")
+    # print(f"Network Address: {network_address}")
+    # print(f"Broadcast Address: {broadcast_address}")
+    # print(f"Usable Address Range: {usable_start} - {usable_end}\n")
