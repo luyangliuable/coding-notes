095c0b2119bf2e39e54136acbe975380c2c18cde
Luyang Liu <luyangl16@gmail.com>
2023-06-16 18:40:18 +1000

    "Removed files and added new algorithms for binary search and jump game."

---
+++ algorithms/leetcode/binary_search.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums, target: int) -> int:
+        hi = len(nums) - 1
+        lo = 0
+
+
+        while lo <= hi:
+            mid = lo + ( hi - lo )//2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+
+        return -1
+
+a = Solution()
+
