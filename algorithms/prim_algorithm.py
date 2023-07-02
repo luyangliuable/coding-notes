a05422fc7412d6d32017b4b4df3a550b3d258a56
Lucas Liu <lucas.liu@wexinc.com>
2023-07-02 13:18:41 +1000

    Added correct prim algorithm and used implemented heapq instead

--- algorithms/prim_algorithm.py
+++ algorithms/prim_algorithm.py
@@ -1,3 +1,6 @@
+import heapq
+import sys
+
 class Graph:
     def __init__(self, g):
         self.g = g
@@ -8,64 +11,67 @@ class Graph:
     def __len__(self):
         return len(self.g)
 
-class Queue:
-    def __init__(self, arr):
-        # This is a minheap
-        self.arr = []
+# class Queue:
+#     def __init__(self, arr):
+#         # This is a minheap
+#         self.arr = []
 
-        for item in arr:
-            self.arr.append(item)
-            self.sift_up(len(self.arr) - 1)
+#         for item in arr:
+#             self.arr.append(item)
+#             self.sift_up(len(self.arr) - 1)
 
-
-    def sift_up(self, i):
-        while (i - 1) // 2 >= 0 and self.arr[ (i-1)//2 ] > self.arr[i]:
-            self.arr[(i-1)//2], self.arr[i] = self.arr[i], self.arr[(i-1)//2]
-            i = (i-1) //2
-
-        return i
-
-    def append(self, v):
-        self.arr.append(v)
-        self.sift_up(len(self.arr) - 1)
-
-    def pop(self):
-        self.arr[0], self.arr[-1] = self.arr[-1], self.arr[0]
-
-        r = self.arr.pop()
-
-        self.sink_down(0)
-
-        return r
-
-    def pop_all(self):
-        while self.arr:
-            print(self.pop())
+#     def __len__(self):
+#         return len(self.arr)
 
 
-    def sink_down(self, i):
-        smallest = i
+#     def sift_up(self, i):
+#         while (i - 1) // 2 >= 0 and self.arr[ (i-1)//2 ] > self.arr[i]:
+#             self.arr[(i-1)//2], self.arr[i] = self.arr[i], self.arr[(i-1)//2]
+#             i = (i-1) //2
 
-        while True:
+#         return i
 
-            left = 2*i + 1
-            right = 2*i + 2
+#     def append(self, v):
+#         self.arr.append(v)
+#         self.sift_up(len(self.arr) - 1)
 
-            if left < len(self.arr) and self.arr[left] < self.arr[smallest]:
-                smallest = left
+#     def pop(self):
+#         self.arr[0], self.arr[-1] = self.arr[-1], self.arr[0]
 
-            if right < len(self.arr) and self.arr[right] < self.arr[smallest]:
-                smallest = right
+#         r = self.arr.pop()
 
-            if smallest == i:
-                break
+#         self.sink_down(0)
 
-            self.arr[smallest], self.arr[i] = self.arr[i], self.arr[smallest]
+#         return r
 
-            i = (smallest)
+#     def pop_all(self):
+#         while self.arr:
+#             print(self.pop())
 
-    def __repr__(self):
-        return str(self.arr)
+
+#     def sink_down(self, i):
+#         smallest = i
+
+#         while True:
+
+#             left = 2*i + 1
+#             right = 2*i + 2
+
+#             if left < len(self.arr) and self.arr[left] < self.arr[smallest]:
+#                 smallest = left
+
+#             if right < len(self.arr) and self.arr[right] < self.arr[smallest]:
+#                 smallest = right
+
+#             if smallest == i:
+#                 break
+
+#             self.arr[smallest], self.arr[i] = self.arr[i], self.arr[smallest]
+
+#             i = (smallest)
+
+#     def __repr__(self):
+#         return str(self.arr)
 
 def print_matrix(matrix):
     for row in matrix:
@@ -74,14 +80,39 @@ def print_matrix(matrix):
         print()
 
 def prim(graph, u):
+    n = len(graph)
+
     visited = [0 for _ in range(len(graph))]
 
-    T = [ [0 for _ in range(len(graph.g[0]))] for _ in range(len(graph.g))]
+    parent = [-1 for _ in range(n)]
 
-    min_heap = Queue([])
-    min_heap.append(u)
+    keys = [sys.maxsize for _ in range(n)]
+    keys[u] = 0
 
-    min_heap.
+    min_heap = []
+    heapq.heappush(min_heap, [0, u, -1])
+
+    while min_heap:
+
+        _, u, _ = heapq.heappop(min_heap)
+
+        # Everything that is popped out of the minheap is confirmed to be finalised
+        visited[u] = 1
+
+        for v in range(n):
+            if visited[v] != 1 and graph.g[u][v] > 0 and keys[v] > graph.g[u][v]:
+                # keys[v] > graph.g[u][v] means that are updating the parent arr if the current path to the vertex is not the minimum
+                # graph.g[u][v] > 0 means if it is 0 or less it is not directly connected
+                # visited[v] != 1 exclude finalised vertex (i.e. edge going in is confirmed to be the min)
+
+                keys[v] = graph.g[u][v]
+                parent[v] = u
+                heapq.heappush(min_heap, [graph.g[u][v], u, v])
+
+    return parent
+
+
+
 
 
 if __name__ == "__main__":
@@ -95,4 +126,4 @@ if __name__ == "__main__":
 
     g = Graph(graph1)
 
-    prim(g, 0)
+    print(prim(g, 0))
