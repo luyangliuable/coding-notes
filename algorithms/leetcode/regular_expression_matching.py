dedcf7399380e09df9906abd521f71a98a378bdd
Lucas Liu <lucas.liu@wexinc.com>
2023-07-03 22:37:11 +1000

    Added top-down approach for regular_expression_matching.py and finished in 5m

--- algorithms/leetcode/regular_expression_matching.py
+++ algorithms/leetcode/regular_expression_matching.py
@@ -26,3 +26,46 @@ class Solution:
                     c[i][j] = c[i+1][j+1]
 
         return c[0][0]
+
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        # Beats 93%!!!!!!!
+        # Top down approach
+        c = {}
+
+        def dfs(i, j):
+            # Base cases
+
+            if (i, j) in c:
+                return c[(i, j)]
+
+            if i >= len(s) and j >= len(p):
+                # Perfect Match
+                return True
+            elif j>= len(p):
+                # Exhausted regex not finished matching s
+                return False
+
+            # match same character and s is not exhausted
+            # Because it is possible p=a*b nad s=aaa
+            match = i < len(s) and ( s[i] == p[j]  or p[j] == ".") # . can match anything
+
+            # There exists an asterick
+            if j + 1 < len(p) and p[j+1] == "*":
+
+                # Either use *, dfs(i+1, j) or no use * dfs(i, j + 2)
+                # If use *, the char proceeding * must match
+                c[(i, j)] = (match and dfs(i+1, j) ) or dfs(i, j+2)
+
+                return c[(i, j)]
+            # There exists no asterick so we just move +1 both p and s
+            elif match:
+                c[(i, j)] = dfs(i+1, j+1)
+                return c[(i, j)]
+
+            # No matches
+            c[(i, j)] = False
+            return False
+
+        return dfs(0, 0)
